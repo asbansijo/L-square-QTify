42f2581b513908cf4a56ed1983ed77b31f1d995f
asbansijo <asban.sijo@gmail.com>
2024-05-31 13:59:02 +0530

    commit

---
+++ qtify/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar.jsx'
+import Hero from './components/Hero/Hero.jsx'
+import styles from './App.module.css'
+import { useEffect, useState } from 'react'
+
+
+function App() {
+  
+  return (
+    <>
+      <Navbar />
+      {/* <Hero />
+      <div className={styles.sectionWrapper}>
+        <Section type='album' title='Top Albums' data={topAlbumSongs} />
+        <Section type='album' title='New Albums' data={newAlbumSongs} />
+        <FilterSection data={newAlbumSongs} type='songFilter' title='Songs' filteredData={filteredData} filteredDataValues={filteredDataValues} value={value} handleChange={handleChange} handleToggle={handleToggle}/>
+      </div> */}
+    </>
+  )
+}
+
+export default App
